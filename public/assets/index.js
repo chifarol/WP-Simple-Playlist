ddf249f22b696a3a22ec1e93e2dd4961f0fb1be4
chifarol <ilodigwechinaza@gmail.com>
2022-09-05 12:47:07 -0700

    completed transpiling

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,89 +1,88 @@
-const spaddTrackButtons = document.querySelectorAll(".sp-add-track");
-const spclearPlaylistButtons = document.querySelectorAll(".sp-clear-playlist");
-const spTogglePlaylistButtons = document.querySelectorAll(
-  ".sp-toggle-playlist"
-);
-let spremoveTrackButtons = document.querySelectorAll(".sp-remove-track");
-let spFormContainer = document.querySelector("#sp-track-container");
-const mColorInput = document.querySelector("#sp-settings-mcolor");
-const tColorInput = document.querySelector("#sp-settings-tcolor");
-const aColorInput = document.querySelector("#sp-settings-acolor");
-const sColorInput = document.querySelector("#sp-settings-scolor");
-const pColorInput = document.querySelector("#sp-settings-pcolor");
+"use strict";
 
-// add new Fieldset DOM
-spaddTrackButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
+var spaddTrackButtons = document.querySelectorAll(".sp-add-track");
+var spclearPlaylistButtons = document.querySelectorAll(".sp-clear-playlist");
+var spTogglePlaylistButtons = document.querySelectorAll(".sp-toggle-playlist");
+var spremoveTrackButtons = document.querySelectorAll(".sp-remove-track");
+var spFormContainer = document.querySelector("#sp-track-container");
+var mColorInput = document.querySelector("#sp-settings-mcolor");
+var tColorInput = document.querySelector("#sp-settings-tcolor");
+var aColorInput = document.querySelector("#sp-settings-acolor");
+var sColorInput = document.querySelector("#sp-settings-scolor");
+var pColorInput = document.querySelector("#sp-settings-pcolor"); // add new Fieldset DOM
+
+spaddTrackButtons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
     e.preventDefault();
-    const lastFieldSet = spFormContainer.lastElementChild;
-    let lastKey;
-    let nextKey;
+    var lastFieldSet = spFormContainer.lastElementChild;
+    var lastKey;
+    var nextKey;
+
     if (lastFieldSet) {
       lastKey = parseInt(lastFieldSet.dataset.key);
       nextKey = lastKey + 1;
     } else {
       nextKey = 1;
     }
-    const newFieldset = `<fieldset data-key='${nextKey}'><div class="sp-toggle"> <h4></h4><span> &#9650;</span></div> <div class="sp-toggle-target"> <input type='text' placeholder=${sp_scripts.newField.p_title} name='sp-tracks[${nextKey}][title]' class="sp-track-title" required/> <input type='text' placeholder=${sp_scripts.newField.p_artiste} name='sp-tracks[${nextKey}][artiste]' class="sp-track-artiste" required/> <div class='sp-input-music-upload-container'>  <input type='text' placeholder=${sp_scripts.newField.p_url} name='sp-tracks[${nextKey}][url]' class="sp-track-url" required/> <button class="sp-upload" >Upload</button>  </div><div class='sp-input-music-upload-container'> <input type='url' placeholder=${sp_scripts.newField.p_image} name='sp-tracks[${nextKey}][pic]' class='sp-track-pic'  /> <button class="sp-upload-pic" >Upload</button>  
-</div> <input type="button" class="sp-remove-track secondary" value="Remove Track"></div></fieldset>`;
+
+    var newFieldset = "<fieldset data-key='".concat(nextKey, "'><div class=\"sp-toggle\"> <h4></h4><span> &#9650;</span></div> <div class=\"sp-toggle-target\"> <input type='text' placeholder=").concat(sp_scripts.newField.p_title, " name='sp-tracks[").concat(nextKey, "][title]' class=\"sp-track-title\" required/> <input type='text' placeholder=").concat(sp_scripts.newField.p_artiste, " name='sp-tracks[").concat(nextKey, "][artiste]' class=\"sp-track-artiste\" required/> <div class='sp-input-music-upload-container'>  <input type='text' placeholder=").concat(sp_scripts.newField.p_url, " name='sp-tracks[").concat(nextKey, "][url]' class=\"sp-track-url\" required/> <button class=\"sp-upload\" >Upload</button>  </div><div class='sp-input-music-upload-container'> <input type='url' placeholder=").concat(sp_scripts.newField.p_image, " name='sp-tracks[").concat(nextKey, "][pic]' class='sp-track-pic'  /> <button class=\"sp-upload-pic\" >Upload</button>  \n</div> <input type=\"button\" class=\"sp-remove-track secondary\" value=\"Remove Track\"></div></fieldset>");
 
     if (lastFieldSet) {
       lastFieldSet.insertAdjacentHTML("afterend", newFieldset);
     } else {
       spFormContainer.innerHTML = newFieldset;
-    }
-    // Re-register necessary DOM elements
+    } // Re-register necessary DOM elements
+
+
     spRegisterRemoveButtons();
     registerTogglers();
     registerTitles();
     registerUploadButtons();
   });
 });
-
 /**
  * assign listener to 'Remove Track' buttons
  */
+
 function spRegisterRemoveButtons() {
   spremoveTrackButtons = document.querySelectorAll(".sp-remove-track");
-  spremoveTrackButtons.forEach((button) => {
-    button.addEventListener(
-      "click",
-      (e) => {
-        e.preventDefault();
-        button.parentElement.parentElement.remove();
-      },
-      { once: true }
-    );
+  spremoveTrackButtons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      button.parentElement.parentElement.remove();
+    }, {
+      once: true
+    });
   });
 }
 /**
  * assign listener to 'Title' fields
  */
+
+
 function registerTitles() {
-  const titleInputs = document.querySelectorAll(".sp-track-title");
-  titleInputs.forEach((title) => {
-    title.addEventListener("input", (e) => {
-      title.parentElement.parentElement
-        .querySelector(".sp-toggle")
-        .querySelector("h4").innerHTML = e.target.value;
+  var titleInputs = document.querySelectorAll(".sp-track-title");
+  titleInputs.forEach(function (title) {
+    title.addEventListener("input", function (e) {
+      title.parentElement.parentElement.querySelector(".sp-toggle").querySelector("h4").innerHTML = e.target.value;
     });
   });
 }
 /**
  * assign listener to 'Remove Track' buttons
  */
-function registerTogglers() {
-  const toggleBars = document.querySelectorAll(".sp-toggle");
-  toggleBars.forEach((toggleBar) => {
-    const toggleBarText = toggleBar.querySelector("h4");
-    const titleText = toggleBar.parentElement
-      .querySelector(".sp-toggle-target")
-      .querySelector(".sp-track-title");
-    toggleBarText.innerHTML = titleText.value;
 
-    toggleBar.addEventListener("click", () => {
-      const target = toggleBar.parentElement.querySelector(".sp-toggle-target");
-      const icon = toggleBar.querySelector("span");
+
+function registerTogglers() {
+  var toggleBars = document.querySelectorAll(".sp-toggle");
+  toggleBars.forEach(function (toggleBar) {
+    var toggleBarText = toggleBar.querySelector("h4");
+    var titleText = toggleBar.parentElement.querySelector(".sp-toggle-target").querySelector(".sp-track-title");
+    toggleBarText.innerHTML = titleText.value;
+    toggleBar.addEventListener("click", function () {
+      var target = toggleBar.parentElement.querySelector(".sp-toggle-target");
+      var icon = toggleBar.querySelector("span");
+
       if (target.style.display == "") {
         target.style.display = "none";
         icon.style.transform = "rotate(180deg)";
@@ -94,16 +93,17 @@ function registerTogglers() {
     });
   });
   return toggleBars;
-}
+} // assign listener to "Toggle ALl" button
 
-// assign listener to "Toggle ALl" button
-spTogglePlaylistButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
+
+spTogglePlaylistButtons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
     e.preventDefault();
-    const toggleBars = registerTogglers();
-    toggleBars.forEach((toggleBar) => {
-      const target = toggleBar.parentElement.querySelector(".sp-toggle-target");
-      const icon = toggleBar.querySelector("span");
+    var toggleBars = registerTogglers();
+    toggleBars.forEach(function (toggleBar) {
+      var target = toggleBar.parentElement.querySelector(".sp-toggle-target");
+      var icon = toggleBar.querySelector("span");
+
       if (target.style.display == "") {
         target.style.display = "none";
         icon.style.transform = "rotate(180deg)";
@@ -113,99 +113,82 @@ spTogglePlaylistButtons.forEach((button) => {
       }
     });
   });
-});
+}); // assign listener to 'Clear Playlist' button
 
-// assign listener to 'Clear Playlist' button
-spclearPlaylistButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
+spclearPlaylistButtons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
     e.preventDefault();
-    let warning = confirm(`${sp_scripts.warning.clear}`);
+    var warning = confirm("".concat(sp_scripts.warning.clear));
+
     if (warning) {
       spFormContainer.innerHTML = "";
     } else {
       return;
     }
   });
-});
+}); // assign listener to 'Upload' button - WP upload modal
 
-// assign listener to 'Upload' button - WP upload modal
 function registerUploadButtons() {
   jQuery(document).ready(function ($) {
-    let spMediaUploader;
+    var spMediaUploader;
     $(".sp-upload").each(function () {
-      let button = $(this);
+      var button = $(this);
+
       function imageUploader(e) {
-        e.preventDefault();
-        // wp.media.frames.fle_frame
+        e.preventDefault(); // wp.media.frames.fle_frame
+
         spMediaUploader = wp.media({
           title: "Select a Song",
-          library: { type: "audio" },
-          button: {
-            text: "Select Song",
+          library: {
+            type: "audio"
           },
-          multiple: false,
+          button: {
+            text: "Select Song"
+          },
+          multiple: false
         });
-
         spMediaUploader.on("select", function () {
-          attachment = spMediaUploader
-            .state()
-            .get("selection")
-            .first()
-            .toJSON();
-
+          attachment = spMediaUploader.state().get("selection").first().toJSON();
           button.parent().children(".sp-track-url").val(attachment.url);
-          let title = button.parent().parent().children(".sp-track-title");
-          let artiste = button.parent().parent().children(".sp-track-artiste");
+          var title = button.parent().parent().children(".sp-track-title");
+          var artiste = button.parent().parent().children(".sp-track-artiste");
 
           if (attachment.title && title.val() === "") {
             title.val(attachment.title);
-            title
-              .parent()
-              .parent()
-              .children(".sp-toggle")
-              .children("h4")
-              .html(attachment.title);
+            title.parent().parent().children(".sp-toggle").children("h4").html(attachment.title);
           }
 
-          if (
-            attachment.meta &&
-            attachment.meta.artist &&
-            artiste.val() === ""
-          ) {
+          if (attachment.meta && attachment.meta.artist && artiste.val() === "") {
             artiste.val(attachment.meta.artist);
           }
         });
-
         spMediaUploader.open();
       }
+
       button.unbind("click");
       button.on("click", imageUploader);
     });
     $(".sp-upload-pic").each(function () {
-      let button = $(this);
+      var button = $(this);
       button.unbind("click");
       button.on("click", function (e) {
-        e.preventDefault();
-        // wp.media.frames.fle_frame
+        e.preventDefault(); // wp.media.frames.fle_frame
+
         spMediaUploader = wp.media({
           title: "Select a Cover Image",
-          library: { type: "image" },
-          button: {
-            text: "Select Cover Image",
+          library: {
+            type: "image"
           },
-          multiple: false,
+          button: {
+            text: "Select Cover Image"
+          },
+          multiple: false
         });
-
         spMediaUploader.on("select", function () {
-          attachment = spMediaUploader
-            .state()
-            .get("selection")
-            .first()
-            .toJSON();
+          attachment = spMediaUploader.state().get("selection").first().toJSON();
           console.log(attachment.url);
           button.parent().children(".sp-track-pic").val(attachment.url);
         });
-
         spMediaUploader.open();
       });
     });
@@ -215,37 +198,33 @@ function registerUploadButtons() {
 spRegisterRemoveButtons();
 registerTogglers();
 registerTitles();
-registerUploadButtons();
+registerUploadButtons(); // assign listener to 'Main Color' input field
 
-// assign listener to 'Main Color' input field
-mColorInput.addEventListener("change", () => {
+mColorInput.addEventListener("change", function () {
   document.querySelector(".cp-container").style.background = mColorInput.value;
   document.querySelector("#cp-polygon").style.background = mColorInput.value;
-});
-// assign listener to 'Track Color' input field
-tColorInput.addEventListener("change", () => {
-  document
-    .querySelectorAll(".cp-track")
-    .forEach((e) => (e.style.background = tColorInput.value));
-});
-// assign listener to 'Accent Color' input field
-aColorInput.addEventListener("change", () => {
-  document.querySelector(".cp-pause-duration").style.background =
-    aColorInput.value;
-  document.querySelector(".cp-track.cp-selected").style.color =
-    aColorInput.value;
+}); // assign listener to 'Track Color' input field
+
+tColorInput.addEventListener("change", function () {
+  document.querySelectorAll(".cp-track").forEach(function (e) {
+    return e.style.background = tColorInput.value;
+  });
+}); // assign listener to 'Accent Color' input field
+
+aColorInput.addEventListener("change", function () {
+  document.querySelector(".cp-pause-duration").style.background = aColorInput.value;
+  document.querySelector(".cp-track.cp-selected").style.color = aColorInput.value;
   document.querySelector(".cp-pause-play svg").style.fill = aColorInput.value;
   document.querySelector("#cp-play-options svg").style.fill = aColorInput.value;
-});
-// assign listener to 'Shadow Color' input field
-sColorInput.addEventListener("change", () => {
-  document.querySelector(".cp-pause-duration").style.boxShadow =
-    "0px 0px 18px 0px " + sColorInput.value;
-});
-// assign listener to 'Primary Color' input field
-pColorInput.addEventListener("change", () => {
-  document
-    .querySelectorAll(".cp-end svg")
-    .forEach((e) => (e.style.fill = pColorInput.value));
+}); // assign listener to 'Shadow Color' input field
+
+sColorInput.addEventListener("change", function () {
+  document.querySelector(".cp-pause-duration").style.boxShadow = "0px 0px 18px 0px " + sColorInput.value;
+}); // assign listener to 'Primary Color' input field
+
+pColorInput.addEventListener("change", function () {
+  document.querySelectorAll(".cp-end svg").forEach(function (e) {
+    return e.style.fill = pColorInput.value;
+  });
   document.querySelector(".cp-container").style.color = pColorInput.value;
 });
--- public/music-player/index.js
+++ public/music-player/index.js
@@ -1,13 +1,13 @@
-const pluginAssetURL =
-  "http://localhost/hypalong/wp-content/plugins/simple-playlist/public/music-player/";
-const audio = document.querySelector("#cp-audio");
-const replayOptions = document.querySelector("#cp-play-options");
-const tracks = document.querySelectorAll(".cp-track-cont");
-let replayState = "replay-all";
-let currentTrackId;
-let trackSrcArray = {};
-let trackIds = [];
+"use strict";
 
+var pluginAssetURL = "http://localhost/hypalong/wp-content/plugins/simple-playlist/public/music-player/";
+var audio = document.querySelector("#cp-audio");
+var replayOptions = document.querySelector("#cp-play-options");
+var tracks = document.querySelectorAll(".cp-track-cont");
+var replayState = "replay-all";
+var currentTrackId;
+var trackSrcArray = {};
+var trackIds = [];
 /**
  * Get music src
  * - Sets value of CurrentTrackId
@@ -16,6 +16,7 @@ let trackIds = [];
  * @param {number} trackId - track index to load
  * @returns {string}
  */
+
 function loadTrack(trackId) {
   currentTrackId = trackId;
   return trackSrcArray[trackId];
@@ -24,86 +25,73 @@ function loadTrack(trackId) {
  * Get HTML element that displays time
  * @returns {object}
  */
+
+
 function getCurrentTimeDisplay() {
-  const currTime = document
-    .querySelector(".cp-selected")
-    .querySelector(".cp-load-play-animation")
-    .querySelector(".durTime");
+  var currTime = document.querySelector(".cp-selected").querySelector(".cp-load-play-animation").querySelector(".durTime");
   return currTime;
 }
 /**
  * Get HTML element with spinning loader for current track
  * @returns {object}
  */
+
+
 function getCurrentTrackLoader() {
-  const currentTrackLoader = document
-    .querySelector(".cp-selected")
-    .querySelector(".cp-track-cont")
-    .querySelector(".cp-image-container")
-    .querySelector(".cp-loading");
+  var currentTrackLoader = document.querySelector(".cp-selected").querySelector(".cp-track-cont").querySelector(".cp-image-container").querySelector(".cp-loading");
   return currentTrackLoader;
 }
 /**
  * Get HTML elemnt with spinning loader
  * @returns {object}
  */
+
+
 function getCurrentTrackDurationSlider() {
-  const durSlider = document
-    .querySelector(".cp-selected")
-    .querySelector(".cp-track-cont")
-    .querySelector(".cp-load-play-animation")
-    .querySelector(".cp-pause-duration-container")
-    .querySelector(".cp-pause-duration");
+  var durSlider = document.querySelector(".cp-selected").querySelector(".cp-track-cont").querySelector(".cp-load-play-animation").querySelector(".cp-pause-duration-container").querySelector(".cp-pause-duration");
   return durSlider;
 }
 /**
  * Get HTML element for track seek background
  * @returns {object}
  */
+
+
 function getCurTrackDurSliderContainer() {
-  const durSliderContainer = document
-    .querySelector(".cp-selected")
-    .querySelector(".cp-track-cont")
-    .querySelector(".cp-load-play-animation")
-    .querySelector(".cp-pause-duration-container");
+  var durSliderContainer = document.querySelector(".cp-selected").querySelector(".cp-track-cont").querySelector(".cp-load-play-animation").querySelector(".cp-pause-duration-container");
   return durSliderContainer;
 }
 /**
  * Get Play/Pause svg HTML element
  * @returns {object}
  */
+
+
 function getPausePlayButton() {
-  const pausePlayButton = document
-    .querySelector(".cp-selected")
-    .querySelector(".cp-load-play-animation")
-    .querySelector(".cp-pause-play");
+  var pausePlayButton = document.querySelector(".cp-selected").querySelector(".cp-load-play-animation").querySelector(".cp-pause-play");
   return pausePlayButton;
 }
 
 audio.addEventListener("timeupdate", DurTime);
 audio.addEventListener("timeupdate", updateProgress);
 audio.addEventListener("ended", effectReplayStatus);
-audio.addEventListener("canplaythrough", () => {
+audio.addEventListener("canplaythrough", function () {
   getCurrentTrackLoader().style.display = "";
-});
+}); // update replay state and icon
 
-// update replay state and icon
-replayOptions.addEventListener("click", (e) => {
+replayOptions.addEventListener("click", function (e) {
   switch (replayState) {
     case "replay-all":
-      replayOptions.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-      <g ><path d="M5 46q-1.2 0-2.1-.9Q2 44.2 2 43V5q0-1.2.9-2.1Q3.8 2 5 2h38q1.2 0 2.1.9.9.9.9 2.1v38q0 1.2-.9 2.1-.9.9-2.1.9Zm9-2 2.1-2.2-4.3-4.3H38v-11h-3v8H11.8l4.3-4.3L14 28l-8 8Zm9.3-14.1h2.45V18H20.5v2.45h2.8ZM10 21.5h3v-8h23.2l-4.3 4.3L34 20l8-8-8-8-2.1 2.2 4.3 4.3H10Z"/>
-      </g></svg>`;
+      replayOptions.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"48\" width=\"48\">\n      <g ><path d=\"M5 46q-1.2 0-2.1-.9Q2 44.2 2 43V5q0-1.2.9-2.1Q3.8 2 5 2h38q1.2 0 2.1.9.9.9.9 2.1v38q0 1.2-.9 2.1-.9.9-2.1.9Zm9-2 2.1-2.2-4.3-4.3H38v-11h-3v8H11.8l4.3-4.3L14 28l-8 8Zm9.3-14.1h2.45V18H20.5v2.45h2.8ZM10 21.5h3v-8h23.2l-4.3 4.3L34 20l8-8-8-8-2.1 2.2 4.3 4.3H10Z\"/>\n      </g></svg>";
       replayState = "replay-once";
       break;
+
     case "replay-once":
-      replayOptions.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-      <g >
-      <path d="M5 46q-1.2 0-2.1-.9Q2 44.2 2 43V5q0-1.2.9-2.1Q3.8 2 5 2h38q1.2 0 2.1.9.9.9.9 2.1v38q0 1.2-.9 2.1-.9.9-2.1.9Zm9-2 2.1-2.2-4.3-4.3H38v-11h-3v8H11.8l4.3-4.3L14 28l-8 8Zm-4-22.5h3v-8h23.2l-4.3 4.3L34 20l8-8-8-8-2.1 2.2 4.3 4.3H10Z"/>
-      </g></svg>`;
+      replayOptions.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"48\" width=\"48\">\n      <g >\n      <path d=\"M5 46q-1.2 0-2.1-.9Q2 44.2 2 43V5q0-1.2.9-2.1Q3.8 2 5 2h38q1.2 0 2.1.9.9.9.9 2.1v38q0 1.2-.9 2.1-.9.9-2.1.9Zm9-2 2.1-2.2-4.3-4.3H38v-11h-3v8H11.8l4.3-4.3L14 28l-8 8Zm-4-22.5h3v-8h23.2l-4.3 4.3L34 20l8-8-8-8-2.1 2.2 4.3 4.3H10Z\"/>\n      </g></svg>";
       replayOptions.querySelector("svg").classList.add("gray");
       replayState = "replay-none";
       break;
+
     case "replay-none":
       replayOptions.querySelector("svg").classList.remove("gray");
       replayState = "replay-all";
@@ -113,80 +101,83 @@ replayOptions.addEventListener("click", (e) => {
       break;
   }
 });
-
 /**
  * implement replay state
  */
+
 function effectReplayStatus() {
   switch (replayState) {
     case "replay-all":
-      let num = trackIds.indexOf(currentTrackId);
-      let nextTrackId;
-      let nextTrackSrc;
+      var num = trackIds.indexOf(currentTrackId);
+      var nextTrackId;
+      var nextTrackSrc;
+
       if (num < trackIds.length - 1) {
         nextTrackId = trackIds[num + 1];
       } else {
         nextTrackId = trackIds[0];
       }
-      document.querySelector(`.cp-selected`).classList.remove("cp-selected");
-      const nextTrackContainer = document.querySelector(
-        `div[data-id='${nextTrackId}']`
-      );
+
+      document.querySelector(".cp-selected").classList.remove("cp-selected");
+      var nextTrackContainer = document.querySelector("div[data-id='".concat(nextTrackId, "']"));
       nextTrackContainer.parentElement.classList.add("cp-selected");
       audio.src = loadTrack(nextTrackId);
       getCurrentTrackLoader().style.display = "flex";
       audio.play();
       break;
+
     case "replay-once":
       audio.src = loadTrack(currentTrackId);
       audio.play();
       break;
+
     case "replay-none":
       break;
 
     default:
       break;
   }
-}
+} // load track on click
 
-// load track on click
-tracks.forEach((track) => {
+
+tracks.forEach(function (track) {
   trackSrcArray[track.dataset.id] = track.dataset.url;
   trackIds.push(track.dataset.id);
-  track.addEventListener("click", (e) => {
+  track.addEventListener("click", function (e) {
     // get outgoing track
-    const outgoingTrack = document.querySelector(".cp-selected");
+    var outgoingTrack = document.querySelector(".cp-selected");
+
     if (outgoingTrack) {
       if (e.target !== getCurTrackDurSliderContainer()) {
-        const playButton = getPausePlayButton();
+        var playButton = getPausePlayButton();
+
         if (track.parentElement != outgoingTrack) {
           // remove 'cp-selected' from class list of outgoing track
           getCurrentTrackLoader().style.display = "";
           outgoingTrack.classList.remove("cp-selected");
-          track.parentElement.classList.add("cp-selected");
-          // get incoming track's id and map to track array
+          track.parentElement.classList.add("cp-selected"); // get incoming track's id and map to track array
 
           audio.src = loadTrack(track.dataset.id);
           getCurrentTrackLoader().style.display = "flex";
           audio.play();
         } else {
           if (audio.paused) {
-            playButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"> <g > <path d="M26.25 38V10H38v28ZM10 38V10h11.75v28Z"/> </g> </svg>`;
+            playButton.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"48\" width=\"48\"> <g > <path d=\"M26.25 38V10H38v28ZM10 38V10h11.75v28Z\"/> </g> </svg>";
             audio.play();
           } else {
             audio.pause();
-            playButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><g ><path d="M16 37.85v-28l22 14Z"/> </g> </svg>`;
+            playButton.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"48\" width=\"48\"><g ><path d=\"M16 37.85v-28l22 14Z\"/> </g> </svg>";
           }
         }
       } else if (e.target == getCurTrackDurSliderContainer()) {
         // audio seeking
-        const durSliderCont = getCurTrackDurSliderContainer();
-        const durSlider = durSliderCont.querySelector(".cp-pause-duration");
+        var durSliderCont = getCurTrackDurSliderContainer();
+        var durSlider = durSliderCont.querySelector(".cp-pause-duration");
         console.log("slider pressed");
-        const width = durSliderCont.clientWidth;
-        const clickX = e.offsetX;
-        const duration = audio.duration;
-        audio.currentTime = (clickX / width) * duration;
+        var width = durSliderCont.clientWidth;
+        var clickX = e.offsetX;
+        var duration = audio.duration;
+        audio.currentTime = clickX / width * duration;
       }
     } else {
       // get new track
@@ -197,20 +188,18 @@ tracks.forEach((track) => {
     }
   });
 });
-
 /**
  * Update Time display HTML element
  */
+
 function DurTime(e) {
-  const { currentTime } = e.srcElement;
+  var currentTime = e.srcElement.currentTime;
   var sec;
-  var sec_d;
+  var sec_d; // define minutes currentTime
 
-  // define minutes currentTime
-  let min = currentTime == null ? 0 : Math.floor(currentTime / 60);
-  min = min < 10 ? "0" + min : min;
+  var min = currentTime == null ? 0 : Math.floor(currentTime / 60);
+  min = min < 10 ? "0" + min : min; // define seconds currentTime
 
-  // define seconds currentTime
   function get_sec(x) {
     if (Math.floor(x) >= 60) {
       for (var i = 1; i <= 60; i++) {
@@ -225,19 +214,21 @@ function DurTime(e) {
     }
   }
 
-  get_sec(currentTime, sec);
+  get_sec(currentTime, sec); // change currentTime DOM
 
-  // change currentTime DOM
-  let currTime = getCurrentTimeDisplay();
+  var currTime = getCurrentTimeDisplay();
   currTime.innerHTML = min + ":" + sec;
 }
-
 /**
  * Update track duration while playing
  */
+
+
 function updateProgress(e) {
-  const { duration, currentTime } = e.srcElement;
-  const progressPercent = (currentTime / duration) * 100;
-  const progress = getCurrentTrackDurationSlider();
-  progress.style.width = `${progressPercent}%`;
+  var _e$srcElement = e.srcElement,
+      duration = _e$srcElement.duration,
+      currentTime = _e$srcElement.currentTime;
+  var progressPercent = currentTime / duration * 100;
+  var progress = getCurrentTrackDurationSlider();
+  progress.style.width = "".concat(progressPercent, "%");
 }
